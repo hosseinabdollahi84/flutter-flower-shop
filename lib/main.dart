d62768589251488fa14c259a063579e114a7c135
hosseinabdollahi84 <hosseinabdol369@gmail.com>
2025-06-04 02:26:33 -0700

    Rename...

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_7/pages/home_page.dart';
+import 'package:flutter_application_7/pages/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
